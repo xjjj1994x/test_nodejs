a5205dd17191b6ea1f789d6ec3578a2e9bdd5801
xjjj1994x <xjjj1994x@naver.com>
2019-07-15 16:05:38 +0900

    second

--- app.js
+++ app.js
@@ -6,11 +6,32 @@ const port = 3000
 
 const path = require('path');
 const router = express.Router();
-router.get('/', function (req, res) {
-res.sendFile(path.join(__dirname + '/index.html'));
+
+router.get('/external', function (req, res) {
+res.sendFile(path.join(__dirname + '/views/external.html'));
 //__dirname : It will resolve to your project folder.
 });
 
+router.get('/stylesheet1', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/stylesheet1.html'));
+    //__dirname : It will resolve to your project folder.
+});
+router.get('/stylesheet2', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/stylesheet1.html'));
+    //__dirname : It will resolve to your project folder.
+});
+router.get('/stylesheet3', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/stylesheet3.html'));
+    //__dirname : It will resolve to your project folder.
+});
+router.get('/stylesheet4', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/stylesheet4.html'));
+    //__dirname : It will resolve to your project folder.
+});
+router.get('/stylesheet1', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/stylesheet1.html'));
+    //__dirname : It will resolve to your project folder.
+});
 app.use('/', router);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
