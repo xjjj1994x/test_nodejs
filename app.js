e962692efe563b4c9b7ab97036228afa86b0aa84
xjjj1994x <xjjj1994x@naver.com>
2019-07-15 15:10:12 +0900

    app

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+//app.get('/', (req, res) => res.send('Hello World!'))
+
+const path = require('path');
+const router = express.Router();
+router.get('/', function (req, res) {
+res.sendFile(path.join(__dirname + '/index.html'));
+//__dirname : It will resolve to your project folder.
+});
+
+app.use('/', router);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use(express.static('public'))
